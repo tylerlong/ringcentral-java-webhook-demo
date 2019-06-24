57bd9fc8dcaab04ee1fb85d074e6a95ba0814581
Tyler Long <tyler4long@gmail.com>
2019-06-24 14:12:36 -0700

    Try setup WebHook

--- README.md
+++ README.md
@@ -1,31 +1,55 @@
-## Ref
+# RingCentral Java WebHook demo
+
+## Part 1: WebHook Server
+
+### Ref
 
 https://www.eclipse.org/jetty/documentation/current/advanced-embedding.html
 
-
-
-## Download Jetty
+### Download Jetty
 
 ```
 curl -o jetty-all-uber.jar https://repo1.maven.org/maven2/org/eclipse/jetty/aggregate/jetty-all/9.4.19.v20190610/jetty-all-9.4.19.v20190610-uber.jar
 ```
 
-
-## Write code
+### Write code
 
 Create file `WebHookServer.java` with source code as shown in this project.
 
-
-## Compile
+### Compile
 
 ```
 mkdir classes
 javac -d classes -cp jetty-all-uber.jar WebHookServer.java
 ```
 
-
-## Run
+### Run
 
 ```
 java -cp classes:jetty-all-uber.jar com.ringcentral.WebHookServer
+```
+
+
+## Part 2: Setup WebHook
+
+### Download RingCentral
+
+```
+wget -O ringcentral.jar https://dl.bintray.com/tylerlong/maven/com/ringcentral/ringcentral/1.0.0-beta10/ringcentral-1.0.0-beta10.jar
+```
+
+### Write code
+
+Create file `SetupWebHook.java` with source code as shown in this project.
+
+### Compile
+
+```
+javac -d classes -cp ringcentral.jar SetupWebHook.java
+```
+
+### Run
+
+```
+java -cp classes:ringcentral.jar com.ringcentral.SetupWebHook
 ```
---
+++ SetupWebHook.java
@@ -0,0 +1,32 @@
+package com.ringcentral;
+
+import java.io.IOException;
+
+import com.ringcentral.RestException;
+import com.ringcentral.definitions.CreateSubscriptionRequest;
+import com.ringcentral.definitions.NotificationDeliveryModeRequest;
+
+public class SetupWebHook
+{
+    public static void main(String[] args) throws IOException, RestException
+    {
+        RestClient rc = new RestClient(
+            System.getenv("RINGCENTRAL_CLIENT_ID"),
+            System.getenv("RINGCENTRAL_CLIENT_SECRET"),
+            System.getenv("RINGCENTRAL_SERVER_URL")
+        );
+        rc.authorize(
+            System.getenv("RINGCENTRAL_USERNAME"),
+            System.getenv("RINGCENTRAL_EXTENSION"),
+            System.getenv("RINGCENTRAL_PASSWORD")
+        );
+        String WEBHOOK_ADDRESS = "";
+        CreateSubscriptionRequest createSubscriptionRequest = new CreateSubscriptionRequest()
+            .eventFilters(new String[]{"/restapi/v1.0/account/~/extension/~/message-store/instant?type=SMS"})
+            .deliveryMode(new NotificationDeliveryModeRequest().transportType("WebHook").address(WEBHOOK_ADDRESS));
+        var result = rc.restapi().subscription().post(createSubscriptionRequest);
+        System.out.println(result.id);
+        System.out.println("WebHook Ready");
+        rc.revoke();
+    }
+}
